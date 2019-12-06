a916d711fb80c1dfca2d78f46e9d4609fb26fdee
Madelyn Romero <m.romero013@gmail.com>
2019-12-06 10:04:36 -0700

    Create student and course seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+student_1 = Student.create(name: "Hermione Granger", age: 11, house: "Gryffindor")
+student_2 = Student.create(name: "Harry Potter", age: 11, house: "Gryffindor")
+student_3 = Student.create(name: "Ron Weasley", age: 11, house: "Gryffindor")
+
+daad = Course.new(name: "Defense Against the Dark Arts")
+transfig = Course.new(name: "Transfiguration")
+herbology = Course.new(name: "Herbology")
+astronomy = Course.new(name: "Astronomy")
+potions = Course.new(name: "Potions")
+muggles = Course.new(name: "Muggle Studies")
